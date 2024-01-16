23d97afff8bfc81c2366d63f477414c65d2e5177
Core Ocean <coreoceantesting@gmail.com>
2024-01-16 17:28:22 +0530

    theme

---
+++ database/migrations/2024_01_16_090919_create_terms_conditions_mst_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('terms_conditions_mst', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('scheme_id')->constrained('scheme_mst');
+            $table->string('rules_regulations')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('terms_conditions_mst');
+    }
+};
---
+++ database/migrations/2024_01_16_091205_create_document_type_mst_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_type_mst', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('scheme_id')->constrained('scheme_mst');
+            $table->string('document_name')->nullable();
+            $table->string('document_initial')->nullable();
+            $table->string('is_required')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_type_mst');
+    }
+};
