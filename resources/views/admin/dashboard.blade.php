eff10c378cf01069db945fa36119d60d8a069a16
coreoceantesting <coreoceantesting@gmail.com>
2024-02-14 16:06:32 +0530

    change user dashboard ui

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -37,56 +37,34 @@
                                      
                                     @foreach ($scheme as $schemes)
 
-                                    <div class="col-lg-4 mb-3">
-                                        <!-- Portlet card -->
-                                        <div class="card h-100" style="border:solid 1px;">
-                                          <div class="card-header d-flex justify-content-center align-items-center h-100  text-white" style="background-color:#8c68cd;">
-                                            <div class="card-widgets">
-                                            </div>
-                                            <h5 class="card-title mb-0 text-dark" style="text-align:center;"> {{ strtoupper($schemes->scheme_name) }}</h5>
-                                          </div>
-                                          <div id="cardCollpase5" class="collapse show">
-                                            <div class="card-body">
-                                              <br>
-                                                <h4 style="text-align:center;">
-                                                    {{ $schemes->scheme_marathi_name }}
-                                                </h4>
-
-                                                  <div class="save-btn text-center">
-                                                      <a href="{{ route('terms_conditions', $schemes->id) }}">
-                                                          <button type="button" class="btn btn-blue btn-rounded width-md waves-effect waves-light" style="background-color:#8c68cd; color:#000000;">Apply &nbsp;<i class="fas fa-check"></i>
-                                                        </button>
-                                                      </a>
-                                                  </div>
-                                              </br>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-
-
-                                    {{-- <div class="col-md-4"> --}}
-                                        <!-- card -->
-                                        {{-- <div class="card card-animate" style="background-color: #8c68cd;color: white;">
-                                            <div class="card-body">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="flex-grow-1">
-                                                        <p class="text-uppercase fw-medium  mb-0" style="text-align: center;font-size: 20px;">{{ $schemes->scheme_name }}</p>
-                                                    </div>
-
+                                        <div class="col-lg-3 mb-3">
+                                            <!-- Portlet card -->
+                                            <div class="card h-100" style="border:solid 1px;">
+                                                <div class="card-header h-20 text-white" style="background-color:#8c68cd;">
+                                                    <div class="card-widgets"></div>
+                                                    <h5 class="card-title mb-0 text-dark" style="text-align:center;"> {{ strtoupper($schemes->scheme_name) }}</h5>
                                                 </div>
-                                                <div class=" mt-4" >
-
-                                                    <div class="mt-3" style="text-align: center;">
-                                                        <a href="{{ route('terms_conditions', $schemes->id) }}" class="btn btn-success">Apply </a>
-
+                                                <div id="cardCollpase5" class="collapse show">
+                                                    <div class="card-body h-50">
+                                                        <h5 style="text-align:center;">
+                                                            {{ $schemes->scheme_marathi_name }}
+                                                        </h5>
                                                     </div>
+                                                    <div class="card-footer border-0 mt-auto">
+                                                        <div class="save-btn text-center">
+                                                            <a href="{{ route('terms_conditions', $schemes->id) }}">
+                                                                <button type="button" class="btn btn-blue btn-rounded width-md waves-effect waves-light" style="background-color:#8c68cd; color:#000000;">Apply &nbsp;<i class="fas fa-check"></i>
+                                                                </button>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
 
-                                                </div> --}}
-                                            {{-- </div><!-- end card body --> --}}
-                                        {{-- </div><!-- end card --> --}}
-                                    {{-- </div><!-- end col --> --}}
+                                        </div>
                                     @endforeach
+                                </div>
 
                                     
                                     
