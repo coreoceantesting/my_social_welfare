309f9acfdfb034281884f6abef1f1fd9383b1410
coreoceantesting <coreoceantesting@gmail.com>
2024-03-12 16:35:08 +0530

    redesign bus consission scheme certificate card

--- resources/views/dmc/bus_concession/generate_certificate.blade.php
+++ resources/views/dmc/bus_concession/generate_certificate.blade.php
@@ -3,110 +3,105 @@
     <x-slot name="heading">Divyang Scheme Application</x-slot>
 
 <section class="content">
-    <div class="body_scroll">
-
-        <div class="container-fluid">
-            <div class="row clearfix">
-                <div class="col-lg-12">
-
-                    <div class="card"  id="divToPrint">
-                        <div class="body" style="padding:60px;">
-                            <div class="row">
-                                <div class="col-md-3" >
-                                    <div class="icon-box">
-                                        <img class="img-fluid " src="{{ asset('admin/images/users/PMC-logo.png') }}" alt="Awesome Image" style="height:100px; width:150px;">
-                                    </div>
-                                </div>
-                                <div class="col-md-8 text-center">
-                                    <h1><strong>Panvel Municipal Corporation </strong></h1>
-                                    <h4><strong>Senior Citizen/Diyang/Student</strong></h4>
-
-                                </div>
-                            </div>
-                            <hr>
-
-                            <div class="row pt-0">
-                                <div class="col-md-3   col-sm-3 ">
-                                   Date :
-                                   {{ \Carbon\Carbon::now()->format('d/m/Y') }}
-                                </div>
-                            </div>
-
-                        <div class="row pt-3">
-                            <div class="col-md-6 col-sm-6">
-                                <p class="mb-0">
-                                    <strong>Name: </strong>{{ $data->f_name }} {{ $data->m_name }} {{ $data->l_name }}
-                                </p>
-                            </div>
-                            <div class="col-md-6 col-sm-6">
-
-                                {{-- <img src="{{ asset('storage/' . $data->candidate_signature) }}" alt="Photo" style="max-width: 100px; height: auto;"> --}}
-                            </div>
-                            <div class="col-md-6 col-sm-6">
-                                <p class="mb-0">
-                                    <strong>Address: </strong>{{ $data->full_address }}
-                                </p>
-                            </div>
-                        </div>
-
-
-                            <div class="row pt-3">
-                                <div class="col-md-4 col-sm-4">
-
-                                    <p class="mb-0">
-                                        <strong>Candidate signature: </strong>
-                                        {{-- <img src="{{ asset('storage/' . $data->candidate_signature) }}" alt="Photo" style="max-width: 100px; height: auto;"> --}}
-                                    </p>
-
-                                </div>
-
-                                <div class="col-md-4 col-sm-4 text-right">
-                                    <p class="mb-0">
-                                        <strong>Age:  </strong>{{ $data->age }}
-                                    </p>
-                                </div>
-                            </div>
-
-
-                            <div class="row pt-3">
-                                <div class="col-md-6 col-sm-6">
-                                    <p class="mb-0">
-                                        <strong>Date of issue of certificate: </strong>{{ date("d/m/Y", strtotime($data->dmc_approval_date)) }}
-                                    </p>
-                                </div>
-
-                                <div class="col-md-6 col-sm-6">
-                                    <img src="{{ asset('storage/' . $data->dmc_sign) }}" alt="Photo" style="max-width: 100px; height: auto;">
-                                    <p class="mb-0">
-                                        <strong>Development Officer </strong><br>
-                                        <strong>Panvel Municipal Corporation </strong>
-                                    </p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="submit-section text-right pt-5" style="float:right;margin-bottom:50px;">
-					    <a href='' class="btn btn-danger btn-lg text-light" >Cancel</a>
-						<button  class="btn btn-success btn-lg" type="button" onClick="printDiv('divToPrint')" ><i class="fa fa-print fa-lg text-light"></i> &nbsp;&nbsp;Print</button>
-					</div>
+    <div class="container">
+        <div class="pass" id="divToPrint">
+            <div class="card" style="border: 1px solid black">
+    
+                <div class="card-header" style="text-align: center;">
+                    <table style="width: 100%;">
+                        <tr>
+                            <td style="width: 10%;">
+                                <!-- Left Logo -->
+                                <img src="{{ asset('admin/images/users/PMC-logo.png') }}" alt="Left Logo" height="100">
+                            </td>
+                            <td style="width: 70%; text-align: center;">
+                                <!-- Company Name and Additional Info -->
+                                <h1><strong>पनवेल महानगरपालिका</strong></h1>
+                                <h4><strong>जेष्ठ नागरिक / दिव्यांग / विद्यार्थी </strong></h4>
+                            </td>
+                            <td style="width: 20%;">
+                                <!-- Any content you want on the right side -->
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+    
+                <div class="card-body">
+                    <table style="width:100%;">
+                        <thead>
+                        <tr>
+                            <!-- Left side with name and address -->
+                            <th style="width: 50%;">
+                            <ul style="list-style-type: none; margin: 10px; padding: 10px;">
+                                <li><strong>नाव : </strong> {{ $data->f_name }} {{ $data->m_name }} {{ $data->l_name }}</li>
+                                <li><strong>पत्ता  : </strong> {{ $data->full_address }}</li>
+                            </ul>
+                            </th>
+                            <!-- Right side with the image -->
+                            <th style="width: 20%;">
+                            <img src="{{ asset('storage/' . $data->candidate_signature) }}" alt="logo" style="max-width: 60%;">
+                            </th>
+                        </tr>
+                        </thead>
+                    </table>
+    
+                    <table style="width:100%;">
+                        <thead>
+                            <tr>
+                            <th style="text-align:left;width: 50%; padding-left:20px">लाभधारक:</th>
+                            <th style="text-align:left">वय : {{$data->age}}</th>
+                            </tr>
+                            <tr>
+                            <th style="text-align:left;padding-left:20px">निर्गमित केल्याचा दिनांक : {{ \Carbon\Carbon::parse($data->dmc_approval_date)->format('Y-m-d') }} </th>
+                            </tr>
+                        </thead>
+                    </table>
+    
+                    <table style="width:100%;">
+                        <thead>
+                          <tr>
+                            <th style="text-align:left;width: 50%; padding-left:20px"">NMMT प्रवाश्यानसाठी वैध</th>
+                            <th>विभाग प्रमुख </th>
+                          </tr>
+                        </thead>
+                    </table>
+                </div>
+    
+            </div>
+    
+            <div class="card" style="border: 1px solid black">
+                <div class="card-body">
+                    <ul style="list-style-type: none;padding-top:5px">
+                        <li><strong>आधार कार्ड नंबर :</strong> {{$data->adhaar_no}} </li>
+                        <li><strong>दूरध्वनी क्रमांक :</strong> {{$data->contact}} </li>
+                    </ul>
+                    <ol>
+                        <li><strong>सदर ओळखपत्र हे हस्तांतरणीय नाही .</strong></li>
+                        <li><strong>सदर ओळखपत्राचा गैरवापर करण्यात येऊ नये .</strong></li>
+                        <li><strong>हे ओळखपत्र सापडल्यास कृपया पनवेल महानगरपालिका समाजकल्याण विभागात जमा करावे . </strong></li>
+                        <li><strong>सदरचे ओळखपत्र हे एक वर्षापर्यंत वैध राहील .</strong></li>
+                    </ol>
                 </div>
             </div>
+        </div>
+
+        <div class="text-center">
+            <a href="{{ route('bus_concession.index') }}" class="btn btn-danger btn-lg text-light" >Cancel</a>
+            <button  class="btn btn-success btn-lg" type="button" onClick="printDiv('divToPrint')" ><i class="fa fa-print fa-lg text-light"></i> &nbsp;&nbsp;Print</button>
         </div>
     </div>
 </section>
 
 </x-admin.layout>
 <script>
-    function printDiv(divName) {
-        $("#print").css("display", "none");
-        var printContents = document.getElementById(divName).innerHTML;
+    function printDiv(divId) {
+        var printContents = document.getElementById(divId).innerHTML;
         var originalContents = document.body.innerHTML;
-        document.body.innerHTML = printContents;
-        window.print();
-        document.body.innerHTML = originalContents;
-        $("#print").css("display", "block");
-        // location.reload();
 
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
     }
 </script>
